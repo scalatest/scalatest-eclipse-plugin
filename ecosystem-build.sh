75b086d728f3572e1e6cdddf4733fb33c086c860
cheeseng <cheeseng@amaseng.com>
2013-12-12 11:02:49 +0800

    Use kepler-nightly-2.11 branch to build for 4.0 milestone for Scala 2.11.

--- ecosystem-build.sh
+++ ecosystem-build.sh
@@ -50,8 +50,14 @@ rm org.scala-ide.sdt.scalatest.source.feature/feature.xml.original
 rm org.scala-ide.sdt.scalatest.tests/META-INF/MANIFEST.MF.original
 rm org.scala-ide.sdt.scalatest/META-INF/MANIFEST.MF.original
 git checkout kepler-nightly
-build scala-ide-4.0-2_11 http://download.scala-ide.org/sdk/e38/scala211/dev/site/ 2.11.0-M7 e38-scala211-4.0 http://download.eclipse.org/releases/juno/
 build scala-ide-4.0 http://download.scala-ide.org/sdk/e38/scala210/dev/site/ 2.10.3 e38-scala210-4.0 http://download.eclipse.org/releases/juno/
+git checkout .
+rm org.scala-ide.sdt.scalatest.feature/feature.xml.original
+rm org.scala-ide.sdt.scalatest.source.feature/feature.xml.original
+rm org.scala-ide.sdt.scalatest.tests/META-INF/MANIFEST.MF.original
+rm org.scala-ide.sdt.scalatest/META-INF/MANIFEST.MF.original
+git checkout kepler-nightly-2.11
+build scala-ide-4.0-2_11 http://download.scala-ide.org/sdk/e38/scala211/dev/site/ 2.11.0-M7 e38-scala211-4.0 http://download.eclipse.org/releases/juno/
 
 cd ${MERGE_TOOL_DIR}
 mvn -Drepo.dest=${TARGET_DIR}/combined -Drepo.source=file://${TARGET_DIR}/e37-scala29-3.0 package
